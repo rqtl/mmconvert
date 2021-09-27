534677901bc0b27265b5174842e55636b421e0ef
Karl Broman <kbroman@gmail.com>
2021-09-27 18:50:24 -0500

    Fix test of mmconvert()

--- tests/testthat/test-mmconvert.R
+++ tests/testthat/test-mmconvert.R
@@ -2,12 +2,13 @@ context("mmconvert")
 
 test_that("mmconvert works in a simple case", {
 
-    expected_result <- structure(list(chr = c("14", "14", "14"),
+    expected_result <- structure(list(marker=c("rs13482072", "rs13482231", "gnf14.117.278"),
+                                      chr = c("14", "14", "14"),
                                       cM_coxV3_ave = c(0.36341, 28.53961, 59.35825),
                                       cM_coxV3_female = c(0.38999, 34.47776, 64.2981),
                                       cM_coxV3_male = c(0.33855, 22.66819, 54.71467),
-                                      bp_grcm39 = c(6738536L, 67215850L, 121955310L)),
-                                      Mbp_grcm39 = c(6.738536, 6.7215850, 121.955310)),
+                                      bp_grcm39 = c(6738536L, 67215850L, 121955310L),
+                                      Mbp_grcm39 = c(6.738536, 67.215850, 121.955310)),
                                  class = "data.frame",
                                  row.names = c("rs13482072", "rs13482231", "gnf14.117.278"))
 
