d99ad16c4f366d8b1b75d9e7d9a4592a37e3e167
Karl Broman <kbroman@gmail.com>
2021-09-27 18:15:51 -0500

    pull apart mmconvert-package from mmconvert

--- R/mmconvert-package.R
+++ R/mmconvert-package.R
@@ -1,4 +1,17 @@
+#' mmconvert: Mouse Map Converter
+#'
+#' Function to convert mouse genome positions between the build 39
+#' physical map and the Cox genetic map
+#' \doi{10.1534/genetics.109.105486}.
+#'
+#' @section Useful links:
+#' - Source at <https://github.com/rqtl/mmconvert>
+#' - BugReports to <https://github.com/rqtl/mmconvert/issues>
+#'
+#' @docType package
+#' @name mmconvert-package
+#'
 #' @keywords internal
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib mmconvert, .registration=TRUE
-"_PACKAGE"
+NULL
