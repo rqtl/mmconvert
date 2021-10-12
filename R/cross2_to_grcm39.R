b0724651e51b7ec20842747448de5eadf23452bf
Karl Broman <kbroman@gmail.com>
2021-10-12 16:17:16 -0500

    Initial stab at cross2_to_grcm39()

---
+++ R/cross2_to_grcm39.R
@@ -0,0 +1,85 @@
+#' Convert a cross2 object to use mouse build GRCm39
+#'
+#' Convert a cross2 object (with genotypes from one of the MUGA
+#' arrays) to use mouse build GRCm39 and the revised Cox map
+#' positions, revising marker order and omitting markers that are not
+#' found.
+#'
+#' @param cross Object of class `"cross2"`, as produced by
+#' [qtl2::read_cross2()]. Must have markers from just one of the MUGA arrays.
+#'
+#' @param array Character string indicating which of the MUGA arrays
+#' was used ("gm" for GigaMUGA, "mm" for MegaMUGA, "mini" for
+#' MiniMUGA, or "muga" for the original MUGA), or "guess" (the
+#' default) to pick the array with the most matching marker names.
+#'
+#' @export
+#'
+#' @return The input `cross` object with markers subset to those in build GRCm39
+#' and with `pmap` and `gmap` replaced with the GRCm39 physical map and
+#' revised Cox genetic map, respectively.
+#'
+#' @seealso [MUGAmaps]
+#'
+#' @examples
+#' \dontrun{
+#' file <- paste0("https://raw.githubusercontent.com/rqtl/",
+#'                "qtl2data/master/DOex/DOex.zip")
+#' DOex <- read_cross2(file)
+#' DOex <- cross2_to_grcm39(DOex)
+#' }
+
+cross2_to_grcm39 <-
+    function(cross, array=c("guess", "gm", "mm", "mini", "muga"))
+{
+    # check that it's cross2
+    if(!inherits(cross, "cross2")) stop('Input cross must have class "cross2"')
+
+    # markers in the cross object
+    markers <- unlist(lapply(cross$geno, colnames))
+
+    array <- match.arg(array)
+    if(array == "guess") {
+        # compare marker names to the four MUGA arrays
+        # use the one with the most matches
+
+        muga_maps <- mmconvert::MUGAmaps
+        n_match <- vapply(muga_maps, function(a,b) sum(b %in% a$marker), 0, markers)
+        if(!any(n_match > 0)) {
+            stop("No markers found in the MUGA arrays")
+        }
+
+        array <- names(n_match)[which.max(n_match)]
+    }
+
+    map <- muga_maps[[array]]
+
+    # number of markers found
+    n_found <- sum(markers %in% map$marker)
+
+    # if no markers found, stop with an error
+    if(n_found == 0) {
+        stop("No markers found in MUGA array")
+    }
+
+    n_notfound <- length(markers) - n_found
+
+    # if more than 5% of markers omitted, give a warning
+    if(n_notfound / length(markers) >= 0.05) {
+        warning("Omitting ", n_notfound, " (", round(n_notfound/length(markers)*100), "%) markers")
+    } else if(n_notfound > 0) {
+        message("Omitting ", n_notfound, " markers")
+    }
+
+    map <- map[map$marker %in% markers, , drop=FALSE]
+
+    # reorder markers in geno
+
+    # reorder markers in founder_geno
+
+    # paste in new genetic map
+
+    # paste in new physical map
+
+    map
+}
